bb7a28dbf9b0a588cbc5c78d357a2d66281316fd
zhning <zhningd@gmail.com>
2016-03-28 20:49:02 +0800

    修改了PostComment实体

--- src/main/java/com/zhning/hibbs/entity/PostComment.java
+++ src/main/java/com/zhning/hibbs/entity/PostComment.java
@@ -3,10 +3,23 @@ package com.zhning.hibbs.entity;
 /**
  * Created by zhning on 2016/2/5.
  */
-public class PostComment extends BaseContent{
+public class PostComment{
+    long id;
     long followId;
+    long userId;
+    String text;
+    long time;
+    int replyCount;
     int floor;
     int state;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public long getFollowId() {
         return followId;
@@ -14,6 +27,38 @@ public class PostComment extends BaseContent{
 
     public void setFollowId(long followId) {
         this.followId = followId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public int getReplyCount() {
+        return replyCount;
+    }
+
+    public void setReplyCount(int replyCount) {
+        this.replyCount = replyCount;
     }
 
     public int getFloor() {
